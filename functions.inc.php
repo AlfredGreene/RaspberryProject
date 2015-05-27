0df0bbeda786ceac4862f049160a69972e0ddcf5
TheBoss107 <thomas.frey@teleport.ch>
2015-05-27 20:01:15 +0200

    Bootstrap template, index.php, functions.inc.php

---
+++ functions.inc.php
@@ -0,0 +1,81 @@
+<?php
+	function htmlHeader($title){
+		print '<!DOCTYPE html>
+		<html lang="en">
+
+		<head>
+
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta name="description" content="">
+			<meta name="author" content="">
+
+			<title>'.$title.'</title>
+
+			<!-- Bootstrap Core CSS -->
+			<link href="css/bootstrap.min.css" rel="stylesheet">
+
+			<!-- Custom CSS -->
+			<link href="css/stylesheet.css" rel="stylesheet">
+		   
+			<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+			<!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
+			<!--[if lt IE 9]>
+				<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+				<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+
+		</head>
+
+		<body>
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Raspberry Pi</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="#">Videostream</a>
+							</li>
+							<li>
+								<a href="#">Motion Detection</a>
+							</li>
+							<li>
+								<a href="#">Externer Stream</a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>';
+	}
+
+	
+	function createStreamDiv($IPAddress){
+	
+		print '<div class="lead">Aktuelle IP-Adresse: '. $IPAddress .'</p>
+		 <div class="videostream">
+			<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org"
+			   width="640"
+			   height="480"
+			   target="http://'.$IPAddress.':8554"
+			   controls="false"
+			</embed>
+		 </div>';
+	
+	}
+	
+?>
---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+	include 'functions.inc.php';
+	
+	session_start();
+	
+	
+	
+	htmlHeader("Startseite");
+		
+	//$localIp = "10.142.126.113"; 	// IP @ GIBM
+	$localIP = "192.168.0.21"; 		// IP @ Home
+?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Live Videostream starten</h1>
+                <p class="lead">Willkommen im Webinterface des Raspberry Pi's.<br>Hier können Sie Streamen etc.'</p>
+				<?php
+					print '<form action="'.$_SERVER["PHP_SELF"].'" method="post" accept-charset="utf-8">
+					<button class="btn btn-default" aria-label="Left Align" name="start">Stream starten</button> 
+					<button class="btn btn-default" aria-label="Left Align" name="stop">Stream stoppen</button>
+					</form>';
+				  
+					if(isset($_POST['start'])){
+						echo "TEST 1";
+						//exec("sudo /usr/bin/killall motion vlc");
+						//exec("raspivid -o - -t 0 -w 800 -h 600 -fps 30 |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264");
+						//exec("sudo bash /var/www/htdocs/RaspberryProject/scripts/videoStream.sh");
+						echo "Läuft";
+						createStreamDiv($localIP);
+					}
+					
+					if(isset($_POST['stop'])){
+						echo "GESTOPPT";
+						exec("sudo /usr/bin/killall motion vlc");
+					}
+					
+					unset($_POST);
+				?>
+				
+			</div>
+			<!--  /.col -->
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
